0feeab126aa19bbfc304f1f01f40ebd0e62cc9f1
stf@apl.it <stf@apl.it>
2015-05-28 19:51:24 +0200

    kill webserver (it's wrong but it gets the job done)

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
   // webserver = require('gulp-webserver');
   webserver = require('gulp-server-livereload');
+var server;
 
 gulp.task('webserver', function() {
-  gulp.src('./')
+  server = gulp.src('./')
     .pipe(webserver({
       livereload: {
         enable: true,
@@ -11,10 +12,13 @@ gulp.task('webserver', function() {
           cb( !(/node_modules/.test(filePath)) && !(/.git/.test(filePath)));
         }
       },
-      directoryListing: true,
+      directoryListing: false,
       open: true
     }));
 });
- 
- 
+
+gulp.task('killwebserver',function() {
+  server.emit('kill');
+});
+
 gulp.task('default', ['webserver']);
